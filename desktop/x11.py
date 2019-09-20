5c6883160b91c2decff29f2eb263fb3456060b23
hwang381 <wanghanjie2012@gmail.com>
2019-09-20 14:58:58 +0800

    process active window at launch

--- desktop/x11.py
+++ desktop/x11.py
@@ -1,5 +1,5 @@
 from contextlib import contextmanager
-from typing import Callable
+from typing import Callable, Optional
 import Xlib
 import Xlib.display
 import psutil
@@ -27,18 +27,27 @@ class X11DesktopAppChangeSource(object):
         self.last_seen_pid = None
         self.callback = callback
 
+    def _get_active_win_pid(self) -> Optional[int]:
+        active_win_id = self.rootScreen.get_full_property(self.NET_ACTIVE_WINDOW, Xlib.X.AnyPropertyType).value[0]
+        with window_obj(self.display, active_win_id) as win_obj:
+            if win_obj:
+                pid = win_obj.get_full_property(self.NET_WM_PID, Xlib.X.AnyPropertyType).value[0]
+                return pid
+
+    def _process_active_win(self):
+        active_win_pid = self._get_active_win_pid()
+        if active_win_pid != self.last_seen_pid:
+            process = psutil.Process(active_win_pid)
+            self.callback(process.exe())
+            self.last_seen_pid = active_win_pid
+
     def start(self):
+        self._process_active_win()
+
         print('Starting to listen X11 desktop app change')
         while True:  # next_event() sleeps until we get an event
             event = self.display.next_event()
             if event.type != Xlib.X.PropertyNotify:
                 continue
             if event.atom == self.NET_ACTIVE_WINDOW:
-                active_win_id = self.rootScreen.get_full_property(self.NET_ACTIVE_WINDOW, Xlib.X.AnyPropertyType).value[0]
-                with window_obj(self.display, active_win_id) as win_obj:
-                    if win_obj:
-                        pid = win_obj.get_full_property(self.NET_WM_PID, Xlib.X.AnyPropertyType).value[0]
-                        if pid != self.last_seen_pid:
-                            process = psutil.Process(pid)
-                            self.callback(process.exe())
-                            self.last_seen_pid = pid
+                self._process_active_win()
